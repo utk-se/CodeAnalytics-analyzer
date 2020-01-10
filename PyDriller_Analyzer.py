7f6bcc559eb910ca7145427cee42730d3b58663d
Jonathan Bryan <jbryan74@Lap332.local>
2020-01-09 19:23:07 -0500

    Working on gathering data from the files

--- PyDriller_Analyzer.py
+++ PyDriller_Analyzer.py
@@ -1,8 +1,11 @@
-from pydriller import RepositoryMining, GitRepository
+#! /usr/bin/env python
+
 import argparse 
 import os
 import lizard
 import json
+from pydriller import RepositoryMining, GitRepository
+import inspect, importlib as implib
 
 '''
 List of dependencies: 
@@ -19,30 +22,75 @@ parser.add_argument('-p', type=str, required=True,
 args = parser.parse_args()
 #-------------------Done setting/getting args--------------------------------------------
 
+def calculate_num_lines(file):
+
+	with open(file) as f:
+		nlines = 0
+		i = 0
+		for i,l in enumerate(f):
+			pass
+	nlines = i+1
+	return nlines
+
+def handle_py_file(file):
+	
+	ftype = "Python"
+	list_of_imports = []
+
+	path, _ = os.path.splitext(file) # path = file path without the extension
+	file_name = path.split('/')[-1] # file_name = the file without the path
+
+	mod = implib.import_module(file_name, path)
+	print(mod)
+	for i in inspect.getmembers(mod, inspect.ismodule):
+		list_of_imports.append(i[0])
+	num_dependencies = len(list_of_imports)
+	return ftype, num_dependencies, list_of_imports
+
+def handle_c_file(file):
+
+	ftype = "C"
+	return ftype
+
+def handle_js_file(file):
+
+	ftype = "Javascript"
+	return ftype
+
+def handle_java_file(file):
+
+	ftype = "Java"
+	return ftype
+
 def main_function():
 
 	files = GitRepository(args.p).files()
 	for file in files:
 		cpp = file.endswith('.cpp')
+		c = file.endswith('.c')
 		py = file.endswith('.py')
 		js = file.endswith('.js')
 		java = file.endswith('.java')
 		
 		if cpp or py or js or java:
 	
-			with open(file) as f:
-				nlines = 0
-				i = 0
-				for i,l in enumerate(f):
-					pass
-			nlines = i+1
-			if py: ftype = "Python"
-			elif cpp: ftype = "C++"
-			elif js: ftype = "Javascript"
-			else: ftype = "Java"
-			print("File: " + str(file))
-			print("File type: " + str(ftype))
-			print('{:>10}'.format("numlines: ") + str(nlines))
+			nlines = calculate_num_lines(file)
+
+			if py: 
+				ftype, num_dependencies, import_list = handle_py_file(file)
+			elif cpp or c: 
+				ftype = handle_c_file(file)
+			elif js: 
+				ftype = handle_js_file(file)
+			else: 
+				ftype = handle_java_file(file)
+
+			print("Filename: " + str(file))
+			print("Language: " + str(ftype))
+			print("numlines: " + str(nlines))
+			print("Imports: ", import_list)
+			print("Number of Dependencies: " + str(num_dependencies))
+			print("\n")
 		
 if __name__ == "__main__":
     main_function()
