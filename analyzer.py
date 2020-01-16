10c86a695eee73da0f9dc20086af52bbc7003dbc
aiden <arutter@utk.edu>
2020-01-15 20:04:50 -0500

    [WIP] Added CppHeaderParser placeholder dict

--- analyzer.py
+++ analyzer.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import lizard
 import json
+import CppHeaderParser
 
 #-------------------Setting/getting the args---------------------------------------------
 parser = argparse.ArgumentParser(description="Arguments for CodeAnalytics-analyzer")
@@ -11,15 +12,52 @@ args = parser.parse_args()
 #-------------------Done setting/getting args--------------------------------------------
 
 def main_function():
+
+    # Empty output objects to be populated & dumped
+    repo_obj = {
+        "file_objs": [],
+        "commit-id": None,
+        "repo-url": None,
+        "num_lines": None,
+        "date": None
+    }
+
+    file_obj = {
+        "num_lines": None,
+        "file_extension": None,
+        "methods": [],
+        "classes": []
+    }
+
+    method_obj = {
+        "parent_file": None,
+        "parent_class": None
+    }
+
+    supported_filetypes = ['py', 'cpp', 'js', 'h', 'java']
     arr = [] # array containing every analyzed file
     for subdir, dirs, files in os.walk(args.p): # go through every file within a given directory
         for file in files:
             file_path = subdir + os.sep + file
-            if file_path.endswith('.cpp') or file_path.endswith('.py') or file_path.endswith('.js') or file_path.endswith('.java'):
+            file_extension = file_path.split('.')[-1]
+            print(file_extension)
+            if file_extension in supported_filetypes:
+
+                # Lizard cyclomatic complexity analysis
                 i = lizard.analyze_file(file_path)
-                print(i.function_list[0].__dict__)
-                my_json = json.dumps(i.function_list[0].__dict__, indent=4)
-                arr.append(my_json)
+                if len(i.function_list) > 0:
+                    print(i.function_list[0].__dict__)
+
+                    # Throw the lizard results into the json file
+                    my_json = json.dumps(i.function_list[0].__dict__, indent=4)
+                    arr.append(my_json)
+
+                # Parse C/C++ from a header file
+                # TODO: See if this works for .cpp files
+                if file_extension is 'h':
+                    header = CppHeaderParser.CppHeader(file_path)
+                    print(header)
+                    break
 
     with open('data.json', 'w') as outfile:
         outfile.write("[\n")
