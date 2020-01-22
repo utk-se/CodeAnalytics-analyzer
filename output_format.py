66a712e84cd9b311173b910f19c44ce92ba0b568
aiden <arutter@utk.edu>
2020-01-22 16:59:34 -0500

    Added file to represent format that will be outputted to json

---
+++ output_format.py
@@ -0,0 +1,26 @@
+# Empty output objects to be populated & dumped
+repo_obj = {
+    "file_objs": [],
+    "num_lines": None,
+    "num_files": 0
+}
+
+file_obj = {
+    "num_lines": None,
+    "file_extension": None,
+    "methods": [],  #  Tuples w/ start and end lines of methods/classes
+    "classes": [],
+    "lines": []
+}
+
+line_obj = {
+    # Proposed usage: [var, func call, loop, conditional, return,
+    # assignment, include/import]
+    # 'is' subject to change. These could be used for different heatmat 'colors'
+    "is": [None, None, None, None, None, None, None],
+    "start_index": None,  # int specifying where line starts
+    "has_tabs": None,  # boolean for existence
+    "end_index": None  # int specifying where line ends
+}
+
+supported_filetypes = ['py', 'cpp', 'js', 'h', 'java']
