d6a611bdd21b22c68ca586d460a3f5e51833e158
aiden <arutter@utk.edu>
2020-01-29 13:21:23 -0500

    Updated file parser

--- analyzer.py
+++ analyzer.py
@@ -4,7 +4,6 @@ import argparse
 import os
 import lizard
 import json
-from pydriller import RepositoryMining, GitRepository
 
 #-------------------Setting/getting the args---------------------------------------------
 parser = argparse.ArgumentParser(description="Arguments for CodeAnalytics-analyzer")
@@ -14,7 +13,13 @@ parser.add_argument('-p', type=str, required=True,
 
 # TODO: replace if statements with logger
 parser.add_argument('-d', action='store_true', help='Enable debugging')
+# TODO: add ignore file
+parser.add_argument('-i', type=str, help="Specifies an ignorefile. ")
+# How much to expand tabs (default: off)
+parser.add_argument('-e', type=int, default=4, help="Specifies number of spaces to represent tabs as.")
+parser.add_argument('-o', type=str, default="outfile.json", help="Specify name of outfile")
 args = parser.parse_args()
+
 #-------------------Done setting/getting args--------------------------------------------
 
 def main():
@@ -28,45 +33,78 @@ def main():
     supported_filetypes = ['py', 'cpp', 'js', 'h', 'java']
     # Walk through all files
     for subdir, dirs, files in os.walk(args.p):  # go through every file within a given directory
+        # Exclude hidden files/directories 
+        # (see https://stackoverflow.com/questions/13454164/os-walk-without-hidden-folders)
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+
         for file in files:
+        
             file_path = subdir + os.sep + file
             file_extension = file_path.split('.')[-1]
+
             # For each file check file type
-            if file_extension in supported_filetypes:
-                repo_obj["num_files"] += 1
-                file_obj = {
-                    "num_lines": 0,
-                    "file_extension": file_extension,
-                    "file_name": file_path,
-                    "methods": [],  #  Pair/Tuples with start and end lines of methods/classes
-                    "classes": [],
-                    "lines": [],
-                    "nloc": None
-                }
+            if file_extension not in supported_filetypes:
+                continue
 
-                i = lizard.analyze_file(file_path)
-                # what's in i?
-                if args.d:
-                    print(i.function_list)
-                    print(i.token_count)
-                    print(i.nloc)
+            repo_obj["num_files"] += 1
+            file_obj = {
+                "num_lines": 0,
+                "file_extension": file_extension,
+                "file_name": file_path,
+                "methods": [],  #  Pair/Tuples with start and end lines of methods/classes
+                "classes": [],
+                "line_objs": [],
+                "nloc": None,
+                "token_count": 0
+            }
 
-                # Go thru each line in the file
-                for line in file:
+            i = lizard.analyze_file(file_path)
+
+            file_obj["token_count"] = i.token_count
+
+            # Append info about methods
+            for func_dict in i.function_list:
+                file_obj["methods"].append(func_dict.__dict__)
+
+            # Go thru each line in the file
+            with open(file_path) as file:
+                for line_num, line in enumerate(file):
+                    
                     file_obj["num_lines"] += 1
+                    # Don't bother with lines that are just a newline
+                    if line == '\n':
+                        continue
 
                     line_obj = {
+                        "index": line_num,
                         "is": [None, None, None, None, None, None, None],
                         "start_index": None,  # int specifying where line starts
-                        "has_tabs": None,  # boolean for existence
-                        "end_index": None  # int specifying where line ends
+                        "num_tabs": 0,  # boolean for existence
+                        "end_index": None,  # int specifying where line ends
+                        "num_spaces": 0,
+                        "len": 0
                     }
-                    
-                    # detecting imports
+                    # detect tabs & spaces
+                    line_obj["num_tabs"] = line.count('\t')
+                    line_obj["num_spaces"] = line.count(' ')
+                    line_obj["len"] = len(line)
+                    line = line.expandtabs(args.e)
+                    # detect start & end index
+                    line_obj["start_index"] = len(line) - len(line.lstrip())
+                    line_obj["end_index"] = len(line) - len(line.rstrip())
+                    # TODO: detecting imports
 
                     # Add line obj to file obj
                     file_obj["line_objs"].append(line_obj)
-                # Add file obj to repo obj
-                repo_obj["file_objs"].append(file_obj)
+            
+            # Add file obj to repo obj
+            repo_obj["file_objs"].append(file_obj)
+
+    # Write the repo object to json
+    with open(args.o, 'w') as outfile:
+        json.dump(repo_obj, outfile)
+
+
 if __name__ == "__main__":
     main()
