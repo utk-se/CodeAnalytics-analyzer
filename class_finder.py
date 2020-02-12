bf3b11e40857b8c6e9877741c6247608983ae5df
jball1997 <39563445+jball1997@users.noreply.github.com>
2020-02-12 12:09:51 -0500

    class finding stuff

--- class_finder.py
+++ class_finder.py
@@ -37,7 +37,7 @@ def find_classes(path, verbose=0, lang='python'):
                             print('found end of class at eof line #', line_no_end + 2)
                         class_tuples.append(tuple([line_no_start, line_no_end + 1]))
         elif lang == 'java':
-            pattern = re.compile("\s*public class [^\s]* {\n")
+            pattern = re.compile("\s*public class [^\s]*\s*{")
             for line_no_start, line in enumerate(content):
                 if pattern.match(line) is not None:
                     if verbose:
@@ -56,5 +56,49 @@ def find_classes(path, verbose=0, lang='python'):
                             if verbose:
                                 print('found end of class at line #', line_no_end + 2)
                             break
+        elif lang == 'c++':
+            pattern = re.compile("\s*(class|struct) [^\s]*\s*{")
+            pattern2 = re.compile("\s*(class|struct) [^\s]*\s*")
+            for line_no_start, line in enumerate(content):
+                if pattern.match(line) is not None or \
+                (pattern2.match(line) is not None and '{' in content[line_no_start + 1]):
+                    if verbose:
+                        print("found class at line #", line_no_start + 1)
+                    # finding end of class
+                    for line_no_end, line2 in enumerate(content[line_no_start + 1:]):
+                        line_no_end += line_no_start
+                        if '};' in line2:
+                            quote_position = line2.find('"')
+                            end_class_position = line2.find("};")
+                            next_quote_position = line2.find('"', quote_position)
+                            if not ((quote_position >= 0 and quote_position < end_class_position)
+                            and (next_quote_position >= 0 and next_quote_position > end_class_position)
+                            and line2.count('"') % 2 == 0):
+                                class_tuples.append(tuple([line_no_start, line_no_end + 1]))
+                                if verbose:
+                                    print('found end of class at line #', line_no_end + 2)
+                                break
+        elif lang == 'c':
+            pattern = re.compile("\s*struct [^\s]*\s*{")
+            pattern2 = re.compile("\s*struct [^\s]*\s*")
+            for line_no_start, line in enumerate(content):
+                if pattern.match(line) is not None or \
+                (pattern2.match(line) is not None and '{' in content[line_no_start+1]):
+                    if verbose:
+                        print("found class at line #", line_no_start + 1)
+                    # finding end of class
+                    for line_no_end, line2 in enumerate(content[line_no_start + 1:]):
+                        line_no_end += line_no_start
+                        if '};' in line2:
+                            quote_position = line2.find('"')
+                            end_class_position = line2.find("};")
+                            next_quote_position = line2.find('"', quote_position)
+                            if not ((quote_position >= 0 and quote_position < end_class_position)
+                            and (next_quote_position >= 0 and next_quote_position > end_class_position)
+                            and line2.count('"') % 2 == 0):
+                                class_tuples.append(tuple([line_no_start, line_no_end + 1]))
+                                if verbose:
+                                    print('found end of class at line #', line_no_end + 2)
+                                break
         return class_tuples
 
