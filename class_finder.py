f099bbcc7c852f51e5dcd72a5e421371a3b3608b
jball1997 <39563445+jball1997@users.noreply.github.com>
2020-02-19 16:01:01 -0500

    better regex

--- class_finder.py
+++ class_finder.py
@@ -9,12 +9,12 @@ def space_counter(string): # counts num spaces before class keyword
             break
     return space_count
 
-def find_classes(path, verbose=0, lang='python'):
+def find_classes(path, lang, verbose=0):
     with open(path, 'r') as f:
         class_tuples = []
         content = f.readlines()
         if lang == 'python':
-            pattern = re.compile("\s*class [^\s]*:\n")
+            pattern = re.compile("\s*class\s\w*:\n")
             for line_no_start, line in enumerate(content):
                 if pattern.match(line) is not None:
                     if verbose:
@@ -37,65 +37,83 @@ def find_classes(path, verbose=0, lang='python'):
                             print('found end of class at eof line #', line_no_end + 2)
                         class_tuples.append(tuple([line_no_start, line_no_end + 1]))
         elif lang == 'java':
-            pattern = re.compile("\s*public class [^\s]*\s*{")
+            pattern = re.compile("\s*public class\s\w*\s*{?\n")
+            pattern2 = re.compile("\s*public class\s\w*\n")
+            pattern3 = re.compile("\s*{")
             for line_no_start, line in enumerate(content):
-                if pattern.match(line) is not None:
-                    if verbose:
-                        print("found class at line #", line_no_start + 1)
-                    # finding end of class
-                    left_curly_count = 1
-                    right_curly_count = 0
-                    for line_no_end, line2 in enumerate(content[line_no_start + 1:]):
-                        line_no_end += line_no_start
-                        if '{' in line2:
-                            left_curly_count += 1
-                        if '}' in line2:
-                            right_curly_count += 1
-                        if left_curly_count == right_curly_count:
-                            class_tuples.append(tuple([line_no_start, line_no_end + 1]))
+                if pattern.match(line) is not None or pattern2.match(line) is not None:
+                    if pattern2.match(line) is not None:
+                        if pattern3.match(content[line_no_start + 1]) is not None:
                             if verbose:
-                                print('found end of class at line #', line_no_end + 2)
-                            break
-        elif lang == 'c++':
-            pattern = re.compile("\s*(class|struct) [^\s]*\s*{")
-            pattern2 = re.compile("\s*(class|struct) [^\s]*\s*")
-            for line_no_start, line in enumerate(content):
-                if pattern.match(line) is not None or \
-                (pattern2.match(line) is not None and '{' in content[line_no_start + 1]):
-                    if verbose:
-                        print("found class at line #", line_no_start + 1)
-                    # finding end of class
-                    for line_no_end, line2 in enumerate(content[line_no_start + 1:]):
-                        line_no_end += line_no_start
-                        if '};' in line2:
-                            quote_position = line2.find('"')
-                            end_class_position = line2.find("};")
-                            next_quote_position = line2.find('"', quote_position)
-                            if not ((quote_position >= 0 and quote_position < end_class_position)
-                            and (next_quote_position >= 0 and next_quote_position > end_class_position)
-                            and line2.count('"') % 2 == 0):
+                                print("found class at line #", line_no_start + 1)
+                            # finding end of class
+                            left_curly_count = 1
+                            right_curly_count = 0
+                            for line_no_end, line2 in enumerate(content[line_no_start + 2:]):
+                                line_no_end += line_no_start
+                                if '{' in line2:
+                                    left_curly_count += 1
+                                if '}' in line2:
+                                    right_curly_count += 1
+                                if left_curly_count == right_curly_count:
+                                    class_tuples.append(tuple([line_no_start, line_no_end + 1]))
+                                    if verbose:
+                                        print('found end of class at line #', line_no_end + 2)
+                                    break
+                    else:
+                        if verbose:
+                            print("found class at line #", line_no_start + 1)
+                        # finding end of class
+                        left_curly_count = 1
+                        right_curly_count = 0
+                        for line_no_end, line2 in enumerate(content[line_no_start + 1:]):
+                            line_no_end += line_no_start
+                            if '{' in line2:
+                                left_curly_count += 1
+                            if '}' in line2:
+                                right_curly_count += 1
+                            if left_curly_count == right_curly_count:
                                 class_tuples.append(tuple([line_no_start, line_no_end + 1]))
                                 if verbose:
                                     print('found end of class at line #', line_no_end + 2)
                                 break
-        elif lang == 'c':
-            pattern = re.compile("\s*struct [^\s]*\s*{")
-            pattern2 = re.compile("\s*struct [^\s]*\s*")
+        elif (lang == 'c' or lang == 'js'):
+            pattern = re.compile("\s*(class|struct)\s\w*\s*{?\n")
+            pattern2 = re.compile("\s*(class|struct)\s\w*\n")
+            pattern3 = re.compile("\s*{")
             for line_no_start, line in enumerate(content):
-                if pattern.match(line) is not None or \
-                (pattern2.match(line) is not None and '{' in content[line_no_start+1]):
-                    if verbose:
-                        print("found class at line #", line_no_start + 1)
-                    # finding end of class
-                    for line_no_end, line2 in enumerate(content[line_no_start + 1:]):
-                        line_no_end += line_no_start
-                        if '};' in line2:
-                            quote_position = line2.find('"')
-                            end_class_position = line2.find("};")
-                            next_quote_position = line2.find('"', quote_position)
-                            if not ((quote_position >= 0 and quote_position < end_class_position)
-                            and (next_quote_position >= 0 and next_quote_position > end_class_position)
-                            and line2.count('"') % 2 == 0):
+                if pattern.match(line) is not None or pattern2.match(line) is not None:
+                    if pattern2.match(line) is not None:
+                        if pattern3.match(content[line_no_start+1]) is not None:
+                            if verbose:
+                                print("found class at line #", line_no_start + 1)
+                            # finding end of class
+                            left_curly_count = 1
+                            right_curly_count = 0
+                            for line_no_end, line2 in enumerate(content[line_no_start + 2:]):
+                                line_no_end += line_no_start
+                                if '{' in line2:
+                                    left_curly_count += 1
+                                if '}' in line2:
+                                    right_curly_count += 1
+                                if left_curly_count == right_curly_count:
+                                    class_tuples.append(tuple([line_no_start, line_no_end + 1]))
+                                    if verbose:
+                                        print('found end of class at line #', line_no_end + 2)
+                                    break
+                    else:
+                        if verbose:
+                            print("found class at line #", line_no_start + 1)
+                        # finding end of class
+                        left_curly_count = 1
+                        right_curly_count = 0
+                        for line_no_end, line2 in enumerate(content[line_no_start + 1:]):
+                            line_no_end += line_no_start
+                            if '{' in line2:
+                                left_curly_count += 1
+                            if '}' in line2:
+                                right_curly_count += 1
+                            if left_curly_count == right_curly_count:
                                 class_tuples.append(tuple([line_no_start, line_no_end + 1]))
                                 if verbose:
                                     print('found end of class at line #', line_no_end + 2)
