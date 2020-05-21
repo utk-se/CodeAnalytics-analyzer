013e573757ff275e9d758f3730fa4a69ab353dff
Ben Klein <robobenklein@gmail.com>
2020-05-20 22:43:15 -0400

    allow imports now

--- caanalyzer/__init__.py
+++ caanalyzer/__init__.py
@@ -9,7 +9,7 @@ Result data is retrieved via analyzer.Analyzer():analyze()
 """
 
 from cadistributor import log
-#from .analyzer import CodeRepo
-#from .__main__ import analyze
+from .analyzer import CodeRepo
+from .__main__ import analyze
 
 __version__ = "0.0.2"
