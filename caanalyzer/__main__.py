e8e7bcb2c3db31ffbb439f1de0e5b99a72ec9fa9
aiden <arutter@utk.edu>
2020-05-27 16:24:12 -0400

    Analyzer now returns the dataframe when run as a module

--- caanalyzer/__main__.py
+++ caanalyzer/__main__.py
@@ -1,22 +1,16 @@
-
-from bson.json_util import loads, dumps
 from cadistributor import log
 # from analyzer import Analyzer
 from .analyzer import CodeRepo
 from .tokens import MethodTokenizer, FileTokenizer, LineTokenizer
-# from .metrics import Length
+from caanalyzer.metrics import width, height, num_tokens
 
 
 def analyze(path):
-    # the_thing = Analyzer(output_raw=False)
-    # output = the_thing.analyze(input_path=path)
-    # output = the_thing.analyze(argv[1], argv[2])
-    repo = CodeRepo('.')
-    repo.index([FileTokenizer, LineTokenizer, MethodTokenizer], {'len': len})
-    # return output
-    # get average length of a file
+    repo = CodeRepo(path)
+    repo.index([FileTokenizer, LineTokenizer, MethodTokenizer], {
+               'size': len, 'width': width, 'height': height, 'num_tokens': num_tokens})
+    return repo.df
 
 
 if __name__ == "__main__":
     log.info("Running analyze on current directory.")
-    print(dumps(analyze(".")))
