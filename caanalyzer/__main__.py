86d0ced1579465d2adec21bc9186ddfeaa06029e
jball1997 <39563445+jball1997@users.noreply.github.com>
2020-06-11 17:19:53 -0400

    testing

--- caanalyzer/__main__.py
+++ caanalyzer/__main__.py
@@ -5,13 +5,12 @@ from cadistributor import log
 # from .analyzer import Repo
 from caanalyzer import analyzer
 import pprint
-import os
 
 def analyze(path, ignorefile=None):
     repo = analyzer.Repo(path, ignorefile, True)
     return repo.export()
 
 if __name__ == "__main__":
-    log.info("Running analyze on. " + os.getcwd())
+    log.info("Running analyzer")
     pp = pprint.PrettyPrinter()
-    pp.pprint(analyze("."))
+    pp.pprint(analyze("..\.."))
