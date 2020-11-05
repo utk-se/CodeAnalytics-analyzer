51f29d0f07a30facf1b70177357b445e27bb6f21
Aiden Rutter <arutter@utk.edu>
2020-11-05 04:01:41 -0500

    added stuff I've been worrking on

---
+++ caanalyzer/heat_plot.py
@@ -0,0 +1,142 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import NonUniformImage
+import matplotlib.colors as colors
+from sys import argv
+
+import pygments
+import pygments.lexers
+import pathlib
+
+from pygments import token
+from pygments.token import Token
+import numpy as np
+
+'''
+Utilities for plotting code features as a heatmap based on a
+code repository dataframe.
+
+Utilities for encoding code features as a lossy heatmap for transmission
+to upstream
+'''
+
+# get the idx's for newlines
+# very c style
+def get_newline_indices(s):
+    last_newline_idx = -1
+    newline_idx_vec = []
+    while True:
+        last_newline_idx = s.find('\n', last_newline_idx+1)
+        if last_newline_idx == -1:
+            break
+        newline_idx_vec.append(last_newline_idx)
+    return newline_idx_vec
+
+def get_tokens_for_file(filepath):
+    """Get the list of tokens for the file.
+
+    If there is no Pygments lexer for the file extension then returns `None`
+
+    We cannot / shall never use a cache here so that we can be 'pure' for
+    the module _foreach_gitfile function to work without a data race.
+    Performance sacrifice is not a primary concern.
+    Also the memory footprint of a cache is impractical for large repos.
+    Any cache that isn't cross-process is even more useless since most modules
+    only get the result for one file once.
+    """
+    path = pathlib.Path(filepath)
+    try:
+        lxr = pygments.lexers.get_lexer_for_filename(path.name)
+    except pygments.util.ClassNotFound as e:
+        return None
+    with path.open('r') as f:
+        try:
+            return lxr.get_tokens_unprocessed(f.read())
+        except UnicodeDecodeError as e:
+            return None
+
+'''
+Written to use pygments, however, it should apply to anything that has the format idx, type, val or len.
+Would be a bit more trivial if pygments indexed their data by line.
+The resulting array is going to be large, ideally results will be aggregated across all repos (just add the np arrays)
+'''
+def file_tokens_to_frequency_count(input_path, src_resolution_x=250,src_resolution_y=2000,
+    target_token_types=pygments.token.STANDARD_TYPES.keys(), 
+    exclude_token_types = [Token.Text.Whitespace, Token.Literal.String.Doc]):
+    token_types = list(set(target_token_types) - set(exclude_token_types))
+    newline_indices = get_newline_indices(input_path)
+    pygment_token_ids = {k:i for i,k in enumerate(token_types)}
+
+    y_start=0
+    static_frequency_array = np.zeros((src_resolution_y, src_resolution_x, len(pygment_token_ids)), dtype=np.int)
+
+    for (idx, tokentype, value) in get_tokens_for_file(input_path):
+
+        # allow for inclusion/exclusion of token types
+        if tokentype not in token_types:
+            continue
+        # pass by newlines until we reach one that
+        # TODO: consider if idx == newline indices 
+        while idx > newline_indices[y_start]:
+            y_start+=1
+
+        y_end=y_start
+        x_start = idx
+        x_end = x_start+len(value)
+
+        if y_start>0:
+            # store the token data in the first line 
+            y_start -= 1
+            x_start -= newline_indices[y_start]+1
+            x_end -= newline_indices[y_start]+1
+        
+        # TODO: consider idx+len == ending newline index
+        # should be true when a newline is present in the value
+        while True:
+            # check that the ending x fits in the dimension - as of now, this information is discarded
+            if x_end > src_resolution_x:
+                x_end = src_resolution_x
+
+            if (y_end >= len(newline_indices)) or (y_end >= src_resolution_y):
+                break
+
+            if idx + len(value) < newline_indices[y_end]:
+                break
+            x_end = newline_indices[y_end]
+            if y_end > 0:
+                x_end -= newline_indices[y_end-1]
+
+            static_frequency_array[y_end][x_start:x_end, pygment_token_ids[tokentype]]+=1
+            x_start = 0
+            y_end+=1
+            
+        static_frequency_array[y_end][x_start:x_end, pygment_token_ids[tokentype]]+=1
+    return static_frequency_array
+
+'''
+Provide a repo path and create a frequency map for an entire repo.
+Note that this is more of an example, as they should be aggregated
+globally, although probably grouped by language
+'''
+def repo_frequency_count():
+    
+'''
+A sample plotting function I've developed to display heatmaps
+'''
+def plot_heatmap_from_frequency(nd_array, ):
+    # y contains line numbers
+    # x contains column numbers
+    ax = plt.gca()
+
+    im = plt.imshow(frequencies, aspect='equal', cmap='inferno',
+        norm=colors.LogNorm(vmin=1, vmax=np.amax(frequencies)))
+    
+    cbar = ax.figure.colorbar(im, ax=ax, orientation="horizontal")
+
+    plt.xticks(np.arange(0, frequencies.shape[1], step=2))
+    plt.yticks(np.arange(0, frequencies.shape[1], step=10))
+    ax.set_title('Clang: Heatmap of indentation levels')
+    ax.set_xlabel('Indentation level')
+    ax.set_ylabel('Line number')
+    plt.grid()
+    plt.show()
+
